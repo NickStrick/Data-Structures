7694a71483cbada8dd45d4849504eff0c008c839
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-19 17:36:02 -0500

    All Heap Test passed!!!

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,3 +1,6 @@
+from unittest.mock import MagicMock
+
+
 class Heap:
     def __init__(self):
         self.storage = []
@@ -8,25 +11,7 @@ class Heap:
         self.storage.append(value)
         # check if larger then parent until in correct position
         i = len(self.storage)-1
-        found = False
-        if i == 0:
-            return
-        while i > 0:
-
-            # edge cases
-            if i == 1 or i == 2:
-                parent = 0
-            else:
-                parent = (i-1)//2
-
-            # check if parent is smaller, yes then swap, no then return
-            if self.storage[i] <= self.storage[parent]:
-                return
-            else:
-                temp = self.storage[i]
-                self.storage[i] = self.storage[parent]
-                self.storage[parent] = temp
-                i == parent
+        self._bubble_up(i)
 
     # * `delete` removes and returns the 'topmost' value from the heap; this method needs to ensure that the heap property is maintained after the topmost element has been removed.
 
@@ -40,18 +25,14 @@ class Heap:
         self.storage[len(self.storage)-1] = temp
 
         # remove the last element
+        rev = self.storage[len(self.storage)-1]
         del self.storage[len(self.storage)-1]
         # run a loop on the first element till its in the right spot
-        found = False
         i = 0
         print('after', self.storage)
 
         self._sift_down(i)
-        # while found == False:
-        #     # print(i)
-        #     found = self._sift_down(i)
-        #     i += 1
-
+        return rev
        # * `get_max` returns the maximum value in the heap _in constant time_.
 
     def get_max(self):
@@ -67,13 +48,30 @@ class Heap:
 # * `_bubble_up` moves the element at the specified index "up" the heap by swapping it with its parent if the parent's value is less than the value at the specified index.
 
     def _bubble_up(self, index):
-        pass
+        i = index
+        # if i == 0:
+        #     return
+        while i >= 0:
+            print('i', i, self.storage[i])
+
+            if i < 3:
+                parent = 0
+            else:
+                parent = (i-1)//2
+            print(parent, self.storage[i], self.storage[parent])
+            # check if parent is smaller, yes then  swap, no then return
+            if self.storage[i] <= self.storage[parent]:
+                return 0
+            else:
+                temp = self.storage[i]
+                self.storage[i] = self.storage[parent]
+                self.storage[parent] = temp
+                i = parent
 
     # * `_sift_down` grabs the indices of this element's children and determines which child has a larger value.
     #     If the larger child's value is larger than the parent's value, the child element is swapped with the parent.
 
     def _sift_down(self, index):
-        # print('b shift', self.storage)
         left_i = (2*index) + 1
         right_i = (2*index) + 2
 
@@ -98,7 +96,6 @@ class Heap:
             return True
 
         current = self.storage[index]
-        # print('c', current, child)
         if current <= self.storage[child]:
             temp = self.storage[index]
             self.storage[index] = self.storage[child]
@@ -131,25 +128,44 @@ heap = Heap()
 # print(heap.get_max(), 10)
 # print(heap.storage)
 
+# heap.insert(6)
+# heap.insert(8)
+# heap.insert(10)
+# heap.insert(9)
+# heap.insert(1)
+# heap.insert(9)
+# heap.insert(9)
+# heap.insert(5)
+# # print(heap.storage)
+# heap.delete()
+# # print(heap.storage)
+# print(heap.get_max(), 9)
+# heap.delete()
+# # print(heap.storage)
+# print(heap.get_max(), 9)
+# heap.delete()
+# # print(heap.storage)
+# print(heap.get_max(), 9)
+# heap.delete()
+# print(heap.get_max(), 8)
+# heap.delete()
+# print(heap.get_max(), 6)
+# heap._bubble_up = MagicMock()
+# heap.insert(5)
+# print(heap._bubble_up.called)
 heap.insert(6)
+heap.insert(7)
+heap.insert(5)
 heap.insert(8)
 heap.insert(10)
-heap.insert(9)
 heap.insert(1)
-heap.insert(9)
-heap.insert(9)
+heap.insert(2)
 heap.insert(5)
-# print(heap.storage)
-heap.delete()
-# print(heap.storage)
-print(heap.get_max(), 9)
-heap.delete()
-# print(heap.storage)
-print(heap.get_max(), 9)
-heap.delete()
-# print(heap.storage)
-print(heap.get_max(), 9)
-heap.delete()
-print(heap.get_max(), 8)
-heap.delete()
-print(heap.get_max(), 6)
+print(heap.storage)
+
+descending_order = []
+
+while heap.get_size() > 0:
+    descending_order.append(heap.delete())
+
+print(descending_order, [10, 8, 7, 6, 5, 5, 2, 1])
